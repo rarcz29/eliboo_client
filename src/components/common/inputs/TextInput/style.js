9542abd3e93f826b1619548c27f0a0e268981783
rarcz29 <rafalcz29@gmail.com>
2021-02-27 20:06:42 +0100

    dry inputs

--- src/components/common/inputs/TextInput/style.js
+++ src/components/common/inputs/TextInput/style.js
@@ -1,39 +1,7 @@
 import styled from 'styled-components';
+import { StyledBaseInput } from '../sharedStyle';
 
 // TODO: disable highlighting input when disabled
 export const StyledInput = styled.input`
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
-    padding-left: 15px;
-    outline: none;
-    resize: none;
-    background-color: ${(props) => props.backgroundColor || 'transparent'};
-    color: ${(props) => props.colorPrimary || 'black'};
-    border: none;
-    border-bottom: ${(props) => props.borderWidth || '1px'} solid
-        ${(props) => props.colorSecondary || 'black'};
-    font-size: ${(props) => props.fontSize || '1rem'};
-    display: ${(props) => props.display};
-    transition: border-color ease-in-out 0.3s;
-
-    &:hover,
-    &:focus {
-        border-color: ${(props) => props.colorPrimary};
-    }
-
-    &::placeholder {
-        /* Chrome, Firefox, Opera, Safari 10.1+ */
-        color: ${(props) => props.colorSecondary};
-        opacity: 1;
-    }
-
-    &:-ms-input-placeholder {
-        /* Internet Explorer 10-11 */
-        color: ${(props) => props.colorSecondary};
-    }
-
-    &::-ms-input-placeholder {
-        /* Microsoft Edge */
-        color: ${(props) => props.colorSecondary};
-    }
+    ${StyledBaseInput}
 `;
